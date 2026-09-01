319bad8e1e11e3d55735c74bc37a8b9b3893b353
fatema-maitham <fatemamaitham1@gmail.com>
2026-09-01 14:01:45 +0300

    step 6 done

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,41 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{js,jsx}'],
+
     extends: [
       js.configs.recommended,
-      reactHooks.configs.flat.recommended,
+      react.configs.recommended,
+      react.configs['jsx-runtime'],
+      reactHooks.configs.recommended,
       reactRefresh.configs.vite,
     ],
+
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/prop-types': 'off',
+      'react/no-unescaped-entities': 'off',
     },
   },
 ])
--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,142 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+// src/App.jsx
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+// Images
+import survivorImg from './assets/0c2d6b.png';
+import scavengerImg from './assets/033a16.png';
+import shadowImg from './assets/262c36.png';
+import trackerImg from './assets/3c1e70.png';
+import sharpshooterImg from './assets/4b2900.png';
+import medicImg from './assets/5a1e02.png';
+import engineerImg from './assets/5e103e.png';
+import brawlerImg from './assets/67060c.png';
+import infiltratorImg from './assets/ac3220.png';
+import leaderImg from './assets/e41f26.png';
+
+const App = () => {
+
+  const [team, setTeam] = useState([]);
+  const [money, setMoney] = useState(100);
+  const [zombieFighters, setZombieFighters] = useState([
+    {
+      id: 1,
+      name: 'Survivor',
+      price: 12,
+      strength: 6,
+      agility: 4,
+      img: survivorImg,
+    },
+    {
+      id: 2,
+      name: 'Scavenger',
+      price: 10,
+      strength: 5,
+      agility: 5,
+      img: scavengerImg,
+    },
+    {
+      id: 3,
+      name: 'Shadow',
+      price: 18,
+      strength: 7,
+      agility: 8,
+      img: shadowImg,
+    },
+    {
+      id: 4,
+      name: 'Tracker',
+      price: 14,
+      strength: 7,
+      agility: 6,
+      img: trackerImg,
+    },
+    {
+      id: 5,
+      name: 'Sharpshooter',
+      price: 20,
+      strength: 6,
+      agility: 8,
+      img: sharpshooterImg,
+    },
+    {
+      id: 6,
+      name: 'Medic',
+      price: 15,
+      strength: 5,
+      agility: 7,
+      img: medicImg,
+    },
+    {
+      id: 7,
+      name: 'Engineer',
+      price: 16,
+      strength: 6,
+      agility: 5,
+      img: engineerImg,
+    },
+    {
+      id: 8,
+      name: 'Brawler',
+      price: 11,
+      strength: 8,
+      agility: 3,
+      img: brawlerImg,
+    },
+    {
+      id: 9,
+      name: 'Infiltrator',
+      price: 17,
+      strength: 5,
+      agility: 9,
+      img: infiltratorImg,
+    },
+    {
+      id: 10,
+      name: 'Leader',
+      price: 22,
+      strength: 7,
+      agility: 6,
+      img: leaderImg,
+    },
+  ]);
+
+
+  const handleAddFighter = (fighter) => {
+    if (money < fighter.price) {
+      console.log('Not enough money to add this fighter.');
+      return;
+    };
+
+    setTeam([...team, fighter]);
+
+    setZombieFighters(
+      zombieFighters.filter((item) => item.id !== fighter.id)
+    );
+
+    setMoney(money - fighter.price);
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h1>Zombie Fighters</h1>
 
-      <div className="ticks"></div>
+      <p>Money: ${money}</p>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <ul>
+        {zombieFighters.map((fighter) => (
+          <li key={fighter.id}>
+            <img src={fighter.img} alt={fighter.name} />
+            <h2>{fighter.name}</h2>
+            <p>Price: ${fighter.price}</p>
+            <p>Strength: {fighter.strength}</p>
+            <p>Agility: {fighter.agility}</p>
+            <button onClick={() => handleAddFighter(fighter)}>Add</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
 
-export default App
+export default App;
